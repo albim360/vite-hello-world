1c6f351bc1d29d9ee53dbc0e5fc9e955b416d9a8
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-28 18:52:44 +0100

    FEAT creato progetto con title

---
+++ src/AppTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import AppTitle from './AppTitle.vue';
+
+const app = createApp(AppTitle);
+app.mount('#app');
